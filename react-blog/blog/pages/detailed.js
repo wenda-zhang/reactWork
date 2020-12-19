d2d3b6295ed524dd227f390a70228afc1f74b0fa
wenda <471780268@qq.com>
2020-12-19 23:59:22 +0800

    详情页面

--- react-blog/blog/pages/detailed.js
+++ react-blog/blog/pages/detailed.js
@@ -3,11 +3,56 @@ import React from 'react'
 import Link from 'next/head'
 import { Button } from 'antd'
 import '../styles/pages/comm.css'
-import { Row,Col,Menu,Icon } from 'antd'
+import { Row, Col, Menu, Icon, Breadcrumb, Affix } from 'antd'
 import Header from '../components/Header'
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import '../styles/pages/detailed.css'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
 
 
 export default function Detailed() {
+
+  let markdown = 
+     '\n' +
+    '# P01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n' +
+    '\`console.log(111)\` \n\n' +
+    '# p02:来个Hello World 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n' +
+    '***\n\n\n' +
+    '# p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '#5 p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '``` var a=11; ```'
+
   return (
     <div className='main'>
       <Head>
@@ -19,12 +64,50 @@ export default function Detailed() {
       <Row className="comm-main" type="flex" justify="center">
 
         <Col className="comm-left" xs={24} sm={24} md={10} lg={10} xl={10} >
-          左侧
+          <div>
+            <div className="bread-div">
+              <Breadcrumb>
+                <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                <Breadcrumb.Item><a href="/">视频列表</a></Breadcrumb.Item>
+                <Breadcrumb.Item><a href="/">文章名</a></Breadcrumb.Item>
+
+              </Breadcrumb>
+            </div>
+            <div>
+              <div className="detailed-title">
+                react实战操练-技术帅blog
+              </div>
+              <div className="list-icon center">
+                <span>2020-12-19</span>
+                <span>视频教程</span>
+                <span>6666人</span>
+              </div>
+              <div className="detailed-content">
+                <ReactMarkdown 
+                  source={markdown}
+                  escapeHtml={false}
+                />
+              </div>
+            </div>
+          </div>
         </Col>
         <Col className="comm-right" xs={0} sm={0} md={14} lg={8} xl={6} >
-          右侧
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
+          <div className="detailed-nav comm-box">
+            <div className="nav-title">文章目录</div>
+            <MarkNav
+              className="article-menu"
+              source={markdown}
+              ordered={false}
+            />
+          </div>
+          </Affix>
         </Col>
       </Row>
+
+      <Footer />
     </div>
   )
 }
