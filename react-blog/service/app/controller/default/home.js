f04f00d98bc1b109b6dfca9cb9edd418ecedcc72
wenda <471780268@qq.com>
2020-12-22 11:01:28 +0800

    服务端提交

---
+++ react-blog/service/app/controller/default/home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    // const { ctx } = this;
+    // ctx.body = 'hi, egg';
+    this.ctx.body = "api接口";
+  }
+  
+}
+
+module.exports = HomeController;
