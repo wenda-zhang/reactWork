5cc0ccaa20f15759f189b0bed2962ab438cd1467
wenda <471780268@qq.com>
2021-01-13 00:14:20 +0800

    添加文章

--- react-blog/admin/src/Pages/AddArticle.js
+++ react-blog/admin/src/Pages/AddArticle.js
@@ -8,6 +8,39 @@ const {TextArea} = Input
 
 export default function AddArticle() {
     
+    const [articleId,setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
+    const [articleTitle,setArticleTitle] = useState('')   //文章标题
+    const [articleContent , setArticleContent] = useState('')  //markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState('预览内容') //html内容
+    const [introducemd,setIntroducemd] = useState()            //简介的markdown内容
+    const [introducehtml,setIntroducehtml] = useState('等待编辑') //简介的html内容
+    const [showDate,setShowDate] = useState()   //发布日期
+    const [updateDate,setUpdateDate] = useState() //修改日志的日期
+    const [typeInfo ,setTypeInfo] = useState([]) // 文章类别信息
+    const [selectedType,setSelectType] = useState(1) //选择的文章类别
+
+    marked.setOptions({
+        renderer: marked.Renderer(),
+        gfm: true,
+        pedantic: false,
+        tables: true,
+        breaks: false,
+        smartList: true,
+        smartypants: false,
+    })
+
+    const changeContent = (e)=> {
+        setArticleTitle(e.target.value);
+        let html = marked(e.target.value);
+        setMarkdownContent(html);
+    }
+
+    const changeIntroduce = (e) =>{
+        setIntroducemd(e.target.value);
+        let html = marked(e.target.value);
+        setIntroducehtml(html);
+    }
+
     return (
         <div>
             <Row gutter={5}>
@@ -32,14 +65,51 @@ export default function AddArticle() {
                             className="markdown-content"
                             rows={35}
                             placeholder="文章内容" 
+                            onChange={changeContent}
                             />
                         </Col>
                         <Col span={12}>
-                            <div className="show-html"></div>
+                            <div className="show-html"
+                                dangerouslySetInnerHTML={{__html: markdownContent}}
+                                >
+                            </div>
                         </Col>
                     </Row>
                 </Col>
-                <Col span={6}></Col>
+                <Col span={6}>
+                    <Row>
+                        <Col span="24">
+                            <Button size="large">
+                                暂存文章
+                            </Button>
+                            &nbsp;&nbsp;
+                            <Button type="primary" size="large">
+                                发布文章
+                            </Button>
+                            <br/><br/>
+                        </Col>
+                        <Col span="24">
+                            <br/>
+                            <TextArea
+                                rows={4}
+                                placeholder="文章简介"
+                                onChange={changeIntroduce}
+                            ></TextArea>
+                            <br/><br/>
+                            <div className="introduce-html"
+                                 dangerouslySetInnerHTML={{__html: introducehtml}}>
+                            </div>
+                        </Col>
+                        <Col span={12}>
+                            <div className="date-select">
+                                <DatePicker 
+                                    placeholder="发布日期"
+                                    size="large"
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </div>
     )
